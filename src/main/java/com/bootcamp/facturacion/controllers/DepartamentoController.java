00fc51a425ea79c4fe1aadadca13896ba5903f10
bootcamp2026sv <bootcamp2026sv@gmail.com>
2026-05-10 10:58:50 -0600

    controller y service de departamento

--- src/main/java/com/bootcamp/facturacion/controllers/DepartamentoController.java
+++ src/main/java/com/bootcamp/facturacion/controllers/DepartamentoController.java
@@ -3,9 +3,8 @@ package com.bootcamp.facturacion.controllers;
 import com.bootcamp.facturacion.models.Departamento;
 import com.bootcamp.facturacion.services.DepartamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +19,13 @@ public class DepartamentoController {
     public List<Departamento> listadoDepartamentos(){
         return servicio.listadoDepartamentos();
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Departamento guardar(@RequestBody Departamento depto){
+        return servicio.guardar(depto);
+    }
+
+
 
 }

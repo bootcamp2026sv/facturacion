fe493d1d5ab975e118d89a2dcc0eb123a76add97
bootcamp2026sv <bootcamp2026sv@gmail.com>
2026-05-16 17:15:53 -0600

    entidad activad economica

--- src/main/java/com/bootcamp/facturacion/models/ActividadEconomica.java
+++ src/main/java/com/bootcamp/facturacion/models/ActividadEconomica.java
@@ -1,7 +1,25 @@
 package com.bootcamp.facturacion.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cat_actividadesEconomicas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ActividadEconomica {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+
+    @Column(name = "codActividad", nullable = false)
     private String codActividad; // no nulo
+
+    @Column(name = "descActividad", nullable = false)
     private String descActividad; // no nulo
 }

dc31cdaceedb7ecd5453e173866e1dc36bf84081
bootcamp2026sv <bootcamp2026sv@gmail.com>
2026-05-17 14:23:55 -0600

    controladores

--- src/main/java/com/bootcamp/facturacion/models/ActividadEconomica.java
+++ src/main/java/com/bootcamp/facturacion/models/ActividadEconomica.java
@@ -21,4 +21,7 @@ public class ActividadEconomica {
 
     @Column(name = "descActividad", nullable = false)
     private String descActividad; // no nulo
+
+    @Column(name = "activo", nullable = false)
+    private boolean activo; // soft delete
 }

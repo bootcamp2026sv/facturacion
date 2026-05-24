f498a6b97ee193d4e30426f8765a5e585fa69487
bootcamp2026sv <bootcamp2026sv@gmail.com>
2026-05-24 11:28:48 -0600

    documentacion

--- src/main/java/com/bootcamp/facturacion/controllers/MunicipioController.java
+++ src/main/java/com/bootcamp/facturacion/controllers/MunicipioController.java
@@ -3,6 +3,11 @@ package com.bootcamp.facturacion.controllers;
 import com.bootcamp.facturacion.models.Municipio;
 import com.bootcamp.facturacion.services.DepartamentoService;
 import com.bootcamp.facturacion.services.MunicipioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +17,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/v1/municipios")
+@Tag(name = "Municipios", description = "Gestion municipios")
 public class MunicipioController {
 
     @Autowired
     private MunicipioService servicio;
 
 
+    @Operation(summary = "Obtener todos los municipios",description = "Devuelve lista con todos los municipios registrados")
+    @ApiResponse(responseCode = "200", description = "Lista obtenida exitosamente")
     @GetMapping
     public List<Municipio> listadoDepartamentos(){
         return servicio.listadoMunicipios();
@@ -26,8 +34,17 @@ public class MunicipioController {
 
 
     //GET http://localhost:8080/api/v1/departamentos/4
+    @Operation(summary = "Obtener un municipio",description = "Devuelve un municipio registrado")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Municipio obtenido exitosamente"),
+            @ApiResponse(responseCode = "404", description = "Municipio no encontrado, revisar x catalogo")
+    })
     @GetMapping("/{id}")
-    public Municipio unDepartamento(@PathVariable Long id){
+    public Municipio unDepartamento(
+            @Parameter(description = "ID del municipio", example = "1")
+            @PathVariable Long id
+    )
+    {
         return servicio.unMunicipio(id);
     }
 
